416690f4bdc29fc690a6f452e3118a6b9678ccee
wuyan <794916356@qq.com>
2019-09-02 15:15:14 +0800

    监控div的值  自动保存

--- document/views.py
+++ document/views.py
@@ -1,10 +1,10 @@
-import docx
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.db import connection, transaction
 from django.contrib.auth.hashers import make_password,check_password
 from document.models import User
-import time  # 引入time模块
+import time  # 引入tim
+# e模块
 import json  # 引入json模块
 import os
 
